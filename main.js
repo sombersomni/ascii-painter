84a87dad8ae8570cd664ef0224fac7ca2bbfaae9
somberSomni <kartunehustla@gmail.com>
2019-03-07 21:41:14 -0600

    fixed image processing

--- main.js
+++ main.js
@@ -4,48 +4,37 @@ window.onload = function () {
 
     createFade(document.getElementById('message'), 1);
     function calcHeight(w, image) {
-    return w/image.width * image.height
+        return w/image.width * image.height
     }
     function drawAscii(data) {
-    console.log(data);
-    let table = document.getElementById('ascii');
-    const bits = 1;
-    const dark = "▓";
-    const mid = "▒";
-    const light = "░";
-    for(var x = 0; x < data.length; x+=bits) {
-        if(data[x] > 0 && data[x] <= 65) {
-        table.innerText += dark;
+        let table = document.getElementById('ascii');
+        const bits = 1;
+        const dark = "▓";
+        const mid = "▒";
+        const light = "░";
+        for(var x = 0; x < data.length; x+=bits) {
+            if(data[x] > 0 && data[x] <= 65) {
+            table.innerText += dark;
+            }
+            else if(data[x] > 65 && data[x] <= 150) {
+            table.innerText += mid;
+            }
+            else {
+            table.innerText += light;
+            }
+            if((x +1) % Math.sqrt(data.length) == 0 && x != 0) {
+            table.innerText += '\n';
+            }
         }
-        else if(data[x] > 65 && data[x] <= 150) {
-        table.innerText += mid;
-        }
-        else {
-        table.innerText += light;
-        }
-        if((x +1) % Math.sqrt(data.length) == 0 && x != 0) {
-        table.innerText += '\n';
-        }
-    }
-    }
-    function createSpinner(target) {
-    anime({
-        targets: target,
-        duration: 2000,
-        rotate: 360,
-        direction: 'alternate',
-        
-        loop: true
-    })
     }
     function createFade(target, opacity) {
-    anime({
-        targets: target,
-        translateX: 10,
-        opacity: opacity,
-        duration: 4000,
-        loop: 1
-    })
+        anime({
+            targets: target,
+            translateX: 10,
+            opacity: opacity,
+            duration: 4000,
+            loop: 1
+        })
     }
 
     function processImageData(colors) {
@@ -59,39 +48,40 @@ window.onload = function () {
     }
 
     function renderImage(image) {
-    let canvas = document.querySelector("canvas");
-    let imageHolder = document.getElementById("imageHolder");
-    const len = imageHolder.children.length;
-    if(len > 0) {
-        for(let i = 0; i < len; i++){
-        imageHolder.removeChild(imageHolder.children[0]);
+        let canvas = document.querySelector("canvas");
+        let imageHolder = document.getElementById("imageHolder");
+        const len = imageHolder.children.length;
+        if(len > 0) {
+            for(let i = 0; i < len; i++){
+            imageHolder.removeChild(imageHolder.children[0]);
+            }
         }
-    }
-    imageHolder.appendChild(image);
-    let ctx = canvas.getContext('2d');
-    const fixedWidth = canvas.width;
-    const scaledDownHeight = calcHeight(fixedWidth, image); //scales down height
-    ctx.drawImage(image, 0,0,fixedWidth, scaledDownHeight);
-    let imageData = ctx.getImageData(0,0,fixedWidth, fixedWidth);
-    let greyScaleData = processImageData(imageData);
-    drawAscii(greyScaleData);
+        imageHolder.appendChild(image);
+        let ctx = canvas.getContext('2d');
+        const fixedWidth = canvas.width;
+        const scaledDownHeight = calcHeight(fixedWidth, image); //scales down height
+        ctx.drawImage(image, 0,0,fixedWidth, scaledDownHeight);
+        let imageData = ctx.getImageData(0,0,fixedWidth, fixedWidth);
+        let greyScaleData = processImageData(imageData);
+        drawAscii(greyScaleData);
     }
 
     fileInput.addEventListener("change", (e) => {
-    let table = document.getElementById('ascii');
-    table.innerText = "";
-        //createFade(document.getElementById('message'));
-    const newFile = URL.createObjectURL(e.target.files[0]);
-    
-        const imgSize = 320;
-    img.setAttribute("src", newFile);
-    img.onload = () => {
-        const scaledImg = calcHeight(imgSize, img); 
-        img.setAttribute("width", img.toString()+"px");
-    img.setAttribute("height", scaledImg.toString()+"px" );
-    window.URL.revokeObjectURL(newFile);
-        renderImage(img);
-    };
+        let table = document.getElementById('ascii');
+        table.innerText = "";
+            //createFade(document.getElementById('message'));
+        const newFile = URL.createObjectURL(e.target.files[0]);
+        
+            const imgSize = 320;
+        img.setAttribute("src", newFile);
+        img.crossOrigin = "Anonymous";
+        img.onload = () => {
+            const scaledImg = calcHeight(imgSize, img); 
+            img.setAttribute("width", img.toString()+"px");
+            img.setAttribute("height", scaledImg.toString()+"px" );
+            window.URL.revokeObjectURL(newFile);
+            renderImage(img);
+        };
 
     });
 }
